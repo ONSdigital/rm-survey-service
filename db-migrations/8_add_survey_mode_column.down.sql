117334ebf6268ff5ebd6da72c6680aa3fb7d8cd4
bkhan2 <mohammed.khan@ons.gov.uk>
2020-08-24 15:42:55 +0100

    adding db-migration scrips

---
+++ db-migrations/8_add_survey_mode_column.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE survey.survey DROP COLUMN surveymode;
---
+++ db-migrations/8_add_survey_mode_column.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE survey.survey ADD COLUMN surveymode character varying(5) NOT NULL;
+
+INSERT INTO survey.survey(surveymode) VALUES('eQ') WHERE shortname IN ('RSI', 'MWSS', 'QBS','MBS')
+INSERT INTO survey.survey(surveymode) VALUES('SEFT') WHERE shortname IN ('BRES', 'AIFDI', 'AOFDI', 'QIFDI', 'QOFDI', 'Sand&Gravel', 'Blocks', 'Bricks', 'PCS', 'ASHE', 'NBS', 'OFATS', 'GovERD')
