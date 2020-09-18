9e24c978c2f78f00cc559b4eca662c9b14b29a42
Bilal Khan <60875003+bkhan2@users.noreply.github.com>
2020-09-18 15:11:09 +0100

    made sure any surveys that arent eQ are SEFT (#93)
    * made sure any surveys that arent eQ are SEFT
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * made sure any surveys that arent eQ are SEFT
    
    * updated the surveymode up script
    
    Co-authored-by: ras-rm-bot <65395820+ras-rm-bot@users.noreply.github.com>

--- db-migrations/8_add_survey_mode_column.up.sql
+++ db-migrations/8_add_survey_mode_column.up.sql
@@ -8,7 +8,5 @@ $$;
 ALTER TABLE survey.survey ADD COLUMN IF NOT EXISTS surveymode survey.surveymode;
 UPDATE survey.survey SET surveymode = 'EQ' WHERE shortname IN ('MBS', 'QSS', 'QCAS', 'RSI', 'MWSS', 'QBS', 'UKIS', 'QPSESLA', 'QPSESPB', 'QPSESCS',
                   'VACS2', 'VACS3', 'VACS4', 'VACS5', 'Ecommerce', 'CAT', 'COVID', 'EPE');
-UPDATE survey.survey SET surveymode = 'SEFT' WHERE shortname IN ('BRES', 'AIFDI', 'AOFDI', 'QIFDI', 'QOFDI',
-                                                                  'Sand&Gravel', 'Blocks', 'Bricks', 'PCS', 'ASHE',
-                                                                  'NBS', 'OFATS', 'GovERD');
+UPDATE survey.survey SET surveymode = 'SEFT' WHERE surveymode IS NULL;
 ALTER TABLE survey.survey ALTER COLUMN surveymode SET NOT NULL;
