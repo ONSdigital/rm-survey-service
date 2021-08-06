95f6aaf76567d0a74de75cf44f87313eadc5c34b
Warren Bailey <warren@warrenbailey.net>
2021-08-06 13:21:57 +0100

    fix db scripts

--- db-migrations/11_rename_types.down.sql
+++ db-migrations/11_rename_types.down.sql
@@ -1,10 +1,10 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'survey_mode') THEN
-        ALTER TYPE survey.survey_mode RENAME TO survey.surveymode;
+    IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'survey_mode') THEN
+        ALTER TYPE survey.survey_mode RENAME TO surveymode;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'survey_type') THEN
-        ALTER TYPE survey.survey_type RENAME survey.surveytype;
+    IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'survey_type') THEN
+        ALTER TYPE survey.survey_type RENAME TO surveytype;
     END IF;
 END
 $$;
--- db-migrations/11_rename_types.up.sql
+++ db-migrations/11_rename_types.up.sql
@@ -1,10 +1,10 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'surveymode') THEN
-        ALTER TYPE survey.surveymode RENAME TO survey.survey_mode;
+    IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'surveymode') THEN
+        ALTER TYPE survey.surveymode RENAME TO survey_mode;
     END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'surveytype') THEN
-        ALTER TYPE survey.surveytype RENAME survey.survey_type;
+    IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'surveytype') THEN
+        ALTER TYPE survey.surveytype RENAME TO survey_type;
     END IF;
 END
 $$;
